b25acc0854d240e02a757cda954d527f2092242c
Garima Bisht <bishtgarima124@gmail.com>
2025-12-12 19:24:05 +0530

    update for the deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE == 'development' ? "http://localhost:5000/api" : '/api';
+const BASE_URL = import.meta.env.MODE == 'development' ? "http://localhost:5000/api" : 'https://productivity-app-z1ei.onrender.com/api';
 const instance = axios.create({
     baseURL: BASE_URL,
 });
